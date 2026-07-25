750ae5742477342d24d742299a499888f4fa7ca0
takumi-kasahara <205691956+takumi-kasahara@users.noreply.github.com>
2026-07-25 18:30:57 +0900

    refactor: remove -Global flag from Import-Module for improved module scoping
    Signed-off-by: takumi-kasahara <205691956+takumi-kasahara@users.noreply.github.com>

--- Modules/Automation.Office.psm1
+++ Modules/Automation.Office.psm1
@@ -1,4 +1,4 @@
 ﻿Get-ChildItem -LiteralPath $PSScriptRoot -Recurse -Filter '*.cs' |
 ForEach-Object { Add-Type -LiteralPath $_.FullName }
 Get-ChildItem -LiteralPath ($PSScriptRoot | Join-Path -ChildPath 'Sources') -Recurse -Filter '*.psm1' |
-ForEach-Object { Import-Module -Name $_.FullName -Global }
+ForEach-Object { Import-Module -Name $_.FullName }
